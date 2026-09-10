054938d483db9f0adc84142b6614c8337696c32f
ojas <ojassahu002@gmail.com>
2026-09-10 17:33:55 +0530

    lec 1 comp

---
+++ javascript/tut 55/script.js
@@ -0,0 +1,17 @@
+console.log("tut 55")
+
+var a = 4;
+var b = 4;
+var c = "harry";
+
+console.log(a +b+ 56);
+
+console.log(typeof a, typeof b, typeof c )
+
+
+let o = {
+    name : "ojas",
+    class : "cse",
+    "salary man": 452211,
+};
+console.log(o)
