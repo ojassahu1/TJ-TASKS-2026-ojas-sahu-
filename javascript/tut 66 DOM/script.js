ad6438f4a71a085b3270e5fc010faa1389e179b8
ojas <ojassahu002@gmail.com>
2026-09-14 10:52:43 +0530

    done

---
+++ javascript/tut 66 DOM/script.js
@@ -0,0 +1,9 @@
+// document object model 
+
+document.title = "docs";
+console.log(document.title);
+document.body.style.backgroundColor = "green";
+
+console.log("hello  ");
+
+document.body.childNodes()
